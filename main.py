f58624fe5384a7b8bed7e58a5c7564e9a4a424d6
checorazza <ceciicorazza@gmail.com>
2026-01-29 18:47:53 -0300

    testing

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ pyautogui.FAILSAFE = True
 
 TARGET_IMAGES = [
     "compra.png",
-    "ensobrar_periodista.png"
+    "ensobrar_periodista.png",
+    "moneda.png",
 ]
 
 LOOP_DELAY = 0.1
@@ -18,15 +19,17 @@ while True:
     try:
         for image in TARGET_IMAGES:
             try:
+                print(f"Buscando {image}...")
                 location = pyautogui.locateOnScreen(image)
 
                 print(f"Encontré {image}")
                 x, y = pyautogui.center(location)
-                pyautogui.moveTo(x, y, duration=MOVE_DURATION)
+                pyautogui.moveTo(x, y)
                 pyautogui.click()
                 break 
 
             except ImageNotFoundException:
+                print(f"No encontré {image}, buscando siguiente...")
                 pass
 
         time.sleep(LOOP_DELAY)
